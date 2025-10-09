5c87143a289762beb89342756d1b1b62baaa71b1
AQStarLilly <kieranjhislop@gmail.com>
2025-10-09 15:07:28 -0300

    removed a comment I didn't need

--- Assets/Systems/Extra Scripts/SpawnPoint.cs
+++ Assets/Systems/Extra Scripts/SpawnPoint.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public class SpawnPoint : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.green;
